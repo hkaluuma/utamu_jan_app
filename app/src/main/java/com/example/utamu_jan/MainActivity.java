06163628eef21e02b594c26fd8cc055c8a0cc25e
Hillary Kaluuma <hkaluuma@idi.co.ug>
2023-03-10 18:12:19 +0300

    initial commit

---
+++ app/src/main/java/com/example/utamu_jan/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.utamu_jan;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    //visibility identifiers (public, protected, private, default)
+    public String notes;
+    protected String teacherNotes;
+    private boolean okay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //we can write our code below
+        String Student;
+        int number;
+    }
+    //our method count
+    private int count(){
+        int num;
+        num = 10;
+        return num; }
+}
